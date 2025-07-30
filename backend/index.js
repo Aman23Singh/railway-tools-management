1304ff81b43e127ac2c49a0d1e3182bb1636086b
Aman23Singh <amansingh23112003@gmail.com>
2025-07-30 20:54:19 +0530

    change in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://railway-tools-management-fne7.vercel.app/', // ✅ replace with your actual frontend URL
+  credentials: true, // ✅ if using cookies or sessions
+}));
 app.use(express.json());
 
 app.use('/', AuthRoutes);
